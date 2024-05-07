314c8f19d1a33668b8a49811843d2066d979c6f6
Tim <tulxoro@hotmail.com>
2024-05-06 21:40:10 -0700

    added credit card mockup

--- src/routes/creditcard/+page.svelte
+++ src/routes/creditcard/+page.svelte
@@ -5,11 +5,11 @@
 
 
 <main class="flex flex-col h-screen items-center">
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold underline pb-10">
         Credit Card Management Page
     </h1>
 
-    <div id="creditcardcontainer" class="grid">
+    <div id="creditcardcontainer" class="grid grid-cols-2 gap-8" >
         <CreditCard />
         <CreditCard />
         <CreditCard />
