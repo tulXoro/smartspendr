17bb432839b5a5188ac0966c48bf65c073bb0741
Vamonosraza <martinezjessy393@gmail.com>
2024-05-09 20:28:08 -0700

    working on Account page

---
+++ src/components/Chart.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Doughnut } from 'svelte-chartjs';
+    import { data } from './data.js';
+  
+    import {
+      Chart as ChartJS,
+      Title,
+      Tooltip,
+      Legend,
+      ArcElement,
+      CategoryScale,
+    } from 'chart.js';
+  
+    ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+  </script>
+  
+  <Doughnut {data} options={{ responsive: true, maintainAspectRatio: false}} />
+  
