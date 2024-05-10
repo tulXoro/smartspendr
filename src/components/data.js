05a33c1c5b314ad035f1064965782fd2211b9f2f
Vamonosraza <martinezjessy393@gmail.com>
2024-05-10 15:47:49 -0700

    add it "Add account page"

--- src/components/data.js
+++ src/components/data.js
@@ -10,12 +10,12 @@ export const data = {
   datasets: [
     {
       data: [300, 50, 100, 40, 120],
-      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#007BFF', '#4D5360'],
       hoverBackgroundColor: [
         '#FF5A5E',
         '#5AD3D1',
         '#FFC870',
-        '#A8B3C5',
+        '#0056b3',
         '#616774',
       ],
     },
