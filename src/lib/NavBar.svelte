d9a7a5ae47f4b33a0cdfc7c8fc646528d9e737af
Tim <tulxoro@hotmail.com>
2024-05-10 19:16:48 -0700

    revert navbar items to just five

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,5 +1,5 @@
 <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-  <div class="grid h-full max-w-lg grid-cols-7 mx-auto">
+  <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
 
       <a href="/transaction" data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
           <svg class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
