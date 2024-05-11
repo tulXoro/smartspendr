19e7e049b9fe72ec37d8e869ffc184ab448d71fe
Vamonosraza <martinezjessy393@gmail.com>
2024-05-11 12:35:46 -0700

    ddfs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -58,7 +58,7 @@
         <input class="input" type="text" placeholder="Enter Username" bind:value={username}>
 		<input class="input" type="text" placeholder="Enter Password" bind:value={password}>
 
-<Button type="submit" typeOf="primary" url='/transaction'>Log In</Button>
+<Button type="submit" typeOf="primary" url=''>Log In</Button>
 	</form>
 	
 	<Button typeOf="signup" url='/register'>Sign Up</Button>
